5a6267bf7a222a0a02837c201c5f95c4e85fa911
T.V.Chinh <chinhtv124@gmail.com>
2023-05-24 23:56:01 +0700

    thêm info

--- main.js
+++ main.js
@@ -164,7 +164,7 @@ function animate_stop(){
   sun.rotateY(0.004);
   mercury.planet.rotateY(0.004);
   venus.planet.rotateY(0.002);
-  earth.planet.rotateY(0.01);
+  // earth.planet.rotateY(0.01);
   moon.planet.rotateY(0.02);
   mars.planet.rotateY(0.018);
   jupiter.planet.rotateY(0.014);
@@ -193,11 +193,90 @@ stopElement.onclick = function() {
   }
 }
 // --------
-
+const info = {
+  mercury: {
+    name: 'Mercury',
+    distance: 57.9,
+    diameter: 4.879,
+    gravity: 3.7,
+    desc: 'Mercury is the smallest planet in our solar system and closest to the Sun'
+  },
+  venus: {
+    name: 'Venus',
+    distance: 108.2,
+    diameter: 12.104,
+    gravity: 8.9,
+    desc: 'Venus is the second planet from the Sun and is known for its thick atmosphere.'
+  },
+  earth: {
+    name: 'Earth',
+    distance: 149.6,
+    diameter: 12.742,
+    gravity: 9.8,
+    desc: 'Earth is our home planet, known for its diverse ecosystems and supporting life.'
+  },
+  mars: {
+    name: 'Mars',
+    distance: 227.9,
+    diameter: 6.779,
+    gravity: 3.7,
+    desc: 'Mars is the fourth planet from the Sun and is often called the "Red Planet" due to its reddish appearance.'
+  },
+  jupiter: {
+    name: 'Jupiter',
+    distance: 778.5,
+    diameter: 139.820,
+    gravity: 24.8,
+    desc: 'Jupiter is the largest planet in our solar system and is known for its massive size and strong magnetic field.'
+  },
+  saturn: {
+    name: 'Saturn',
+    distance: 1.4,
+    diameter: 116.460,
+    gravity: 10.4,
+    desc: 'Saturn is famous for its beautiful rings, which are made up of ice particles and rocks.'
+  },
+  uranus: {
+    name: 'Uranus',
+    distance: 2.9,
+    diameter: 50.724,
+    gravity: 8.7,
+    desc: 'Uranus is a gas giant with a unique feature of being tilted on its side, causing its rings and moons to orbit in an unusual manner.'
+  },
+  neptune: {
+    name: 'Neptune',
+    distance: 4.5,
+    diameter: 49.244,
+    gravity: 11.2,
+    desc: 'Neptune is the farthest planet from the Sun and is known for its striking blue color and strong winds.'
+  },
+  pluto: {
+    name: 'Pluto',
+    distance: 5.9,
+    diameter:  2.377,
+    gravity: 0.62,
+    desc: 'Pluto is a small and icy dwarf planet located in the outer regions of the solar system. It was discovered in 1930 and was considered the ninth planet until its reclassification in 2006. Pluto has a highly elliptical orbit and a thin atmosphere primarily composed of nitrogen, methane, and carbon monoxide. It has five known moons, the largest of which is Charon.'
+  },
+  sun: {
+    name: 'Sun',
+    distance: 149.6 ,
+    diameter:  1.39,
+    gravity: 274,
+    desc: "The Sun is a yellow dwarf star, primarily composed of hydrogen and helium. It is incredibly massive and accounts for about 99.86% of the total mass in the solar system. The Sun generates energy through nuclear fusion, where hydrogen atoms combine to form helium, releasing a tremendous amount of energy in the process. This energy radiates outward in the form of sunlight, providing heat and light to the planets in the solar system. The Sun's intense gravity holds the solar system together and influences the orbital paths of planets and other celestial bodies."
+  },
+  moon:{
+    name: 'Moon',
+    distance: 0.3844,
+    diameter:  1.622,
+    gravity: 274,
+    desc: "The Moon is Earth's only natural satellite and is the fifth-largest satellite in the solar system. It has a rocky surface covered with craters, mountains, valleys, and plains. The Moon's gravitational pull influences ocean tides on Earth. It takes about 27.3 days for the Moon to complete one orbit around Earth, which is roughly the same amount of time it takes to complete one rotation on its axis. The Moon does not have an atmosphere and has extreme temperature variations between its day and night sides."
+  }
+}
 // console.log(scene.children)
 const raycaster = new THREE.Raycaster();
 const pointer = new THREE.Vector2();
 var draggable = new THREE.Object3D;
+const getContainer = document.querySelector('.container-info')
 window.addEventListener('click', event => {
   pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
 	pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
@@ -207,10 +286,24 @@ window.addEventListener('click', event => {
 
   if(intersects.length > 0){
     draggable = intersects[0].object;
-    console.log('tim thấy', draggable.name)
-  }
+
+    const html = `
+      <div><span class="title">Name:</span> ${info[draggable.name].name}</div>
+      <div><span class="title">Distance from Sun:</span> ${info[draggable.name].distance} million km</div>
+      <div><span class="title">Diameter:</span> ${info[draggable.name].diameter}km</div>
+      <div><span class="title">Gravity:</span> ${info[draggable.name].gravity} m/s²</div>
+      <div><span class="title">Description:</span> ${info[draggable.name].desc} </div>
+`
+    getContainer.style.display = "block"
+    getContainer.innerHTML = html;
+
+}
 
 })
+
+
+
+
 
 window.addEventListener('resize', function () {
   camera.aspect = window.innerWidth / window.innerHeight;
